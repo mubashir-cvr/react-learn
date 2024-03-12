d191447e8fe67fff798e794b5c8c65735ae6d403
mubashir-cvr <mubashircheruvayoor@gmail.com>
2024-03-12 23:38:40 +0530

    Back to createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 
 import './App.css'
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements } from "react-router-dom";
 import HomePage from "./component/HomePage";
 import Products from "./component/Products";
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: < Products/> }
-// ]);
-const routrDefnitions = createRoutesFromElements(
-  <Route>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/products' element={<HomePage/>}/>
-  </Route>
-)
-const router = createBrowserRouter(routrDefnitions);
+ const router = createBrowserRouter([
+   { path: "/", element: <HomePage /> },
+   { path: "/products", element: < Products/> }
+ ]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/component/common_layouts/RootLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RootLayout() {
+  return (
+    <h1>RootLayout</h1>
+  )
+}
+
+export default RootLayout
